287f361090bb0f60d4c4ea99b0a125907a73acd7
Daniel Kafer <Daniel.J.Kafer@Gmail.com>
2018-11-24 00:39:11 -0600

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app>
+    <v-toolbar
+      app
+      tabs
+      color="white"
+    >
+      <v-tabs
+        slot="extension"
+        light
+        slider-color="green"
+        right
+      >
+        <v-tab
+          v-for="(link, i) in links"
+          :key="i"
+        >
+          <nuxt-link :to="link.to">{{ link.title }}</nuxt-link>
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        fixed: false,
+        links: [
+          { title: 'Home', to: '/' },
+          { title: 'About', to: '/about' },
+          { title: 'Contact', to: '/contact' }
+        ],
+        title: 'Ochs Tree Farm'
+      }
+    }
+  }
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout
+    align-center
+    justify-center
+    row
+    fill-height
+  >
+    <div class="container">
+      <h1 class="display-4" fluid>Ochs Tree Farm</h1>
+      <h2 class="display-3">Galena, IL</h2>
+    </div>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+}
+</script>
+
+<style>
+  .container {
+    text-align: center;
+  }
+</style>
+
